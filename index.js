ffe6d9619a1eaea22424c0ee4d2abddb326dd82e
Tyler Esselman <tme91302@gmail.com>
2022-09-21 22:33:23 -0700

    wrote writetofile and first part if init functions

--- index.js
+++ index.js
@@ -54,10 +54,18 @@ const questions = [{
 }];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) =>
+            err ? console.error(err) : console.log('Success!')
+    ) 
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer
+        .prompt(questions)
+        
+}
 
 // Function call to initialize app
 init();
